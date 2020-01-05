e95ba27c4d1875434e585d9e1e417e0b418a157f
sonic6 <alshihabi.ahmad@gmail.com>
2020-01-05 17:59:05 +0100

    Started project
    Created a new scene "Ahmad Scene"
    Created car roll scene and did som programming for the car movement

---
+++ Unequal life simulator/Assets/Scripts/GenderRandomizor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenderRandomizor : MonoBehaviour {
+
+public bool gender;
+
+	// Use this for initialization
+	void Start () {
+		int rnd = Random.Range(1,3); //3 never shows up
+		if(rnd == 1) //1 for male
+			gender = true;
+		else gender = false; //2 for female
+	
+		print(rnd);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
