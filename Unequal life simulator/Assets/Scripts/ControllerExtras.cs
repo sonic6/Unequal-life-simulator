af77b24a3dbd7946337f6a83b759559a66b26a6b
sonic6 <alshihabi.ahmad@gmail.com>
2020-01-07 11:13:16 +0100

    Adding car scene
    Car scene level in progress

---
+++ Unequal life simulator/Assets/Scripts/ControllerExtras.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class ControllerExtras : MonoBehaviour
+{
+    public void FlipSprite()
+    {
+        GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
+    }
+}
---
+++ Unequal life simulator/Assets/Scripts/DestroyTile.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class DestroyTile : MonoBehaviour
+{
+    public GameObject player;
+
+    RaycastHit2D hit;
+
+    
+
+    void CastRay()
+    {
+        hit = Physics2D.Raycast(player.transform.position, transform.forward);
+        Debug.DrawRay(player.transform.position, transform.forward, Color.red);
+
+        
+        if (hit.collider == null)
+        {
+            player.GetComponent<PlayMakerFSM>().SendEvent("Lose");
+        }
+    }
+
+
+    private void Update()
+    {
+        CastRay();
+    }
+}
