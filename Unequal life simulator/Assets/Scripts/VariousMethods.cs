04e89014b48ade1be2813a231158b876b4ccf84e
sonic6 <alshihabi.ahmad@gmail.com>
2020-01-09 22:39:23 +0100

    Finished up child games

--- Unequal life simulator/Assets/Scripts/VariousMethods.cs
+++ Unequal life simulator/Assets/Scripts/VariousMethods.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using HutongGames.PlayMaker;
 
 public class VariousMethods : MonoBehaviour
 {
@@ -7,7 +9,7 @@ public class VariousMethods : MonoBehaviour
         GetComponent<PlayMakerFSM>().FsmVariables.GetFsmGameObject("me").Value = gameObject;
     }
 
-    public void CheckChild()
+    public void CheckChild() //manages clothes placement on doll in doll level
     {
         VariousMethods[] childs = transform.parent.GetComponentsInChildren<VariousMethods>();
         for (int i = 0; i < childs.Length -1; i++)
@@ -20,5 +22,52 @@ public class VariousMethods : MonoBehaviour
                 otherCloth.transform.parent = null;
             }
         }
+    }
+
+    public void JudgeClothing(int outfit)
+    {
+        int formal = 0;
+        int casual = 0;
+        int princess = 0;
+        
+
+        Transform[] children = gameObject.GetComponentsInChildren<Transform>();
+
+        foreach(Transform child in children)
+        {
+            if(child.name.Contains("formal") || child.name.Contains("slippers"))
+            {
+                formal++;
+            }
+            if (child.name.Contains("princess") || child.name.Contains("slippers"))
+            {
+                princess++;
+            }
+            if (child.name.Contains("casual") || child.name.Contains("boots") || child.name.Contains("slippers"))
+            {
+                casual++;
+            }
+        }
+
+        int[] selected = { casual, princess, formal }; //important that they stay in this order to work with fsm
+        int score = selected[outfit];
+        FsmVariables.GlobalVariables.FindVariable("finalScore").RawValue = score;
+
+    }
+
+    public void CountFCoins(GameObject coinText) //to get coins from doll game
+    {
+        int current = int.Parse(coinText.GetComponent<Text>().text);
+        int gained = PlayerPrefs.GetInt("F_money");
+
+        coinText.GetComponent<Text>().text = (current + gained).ToString();
+    }
+
+    public void CountMCoins(GameObject coinText) //to get coins from car game
+    {
+        int current = int.Parse(coinText.GetComponent<Text>().text);
+        int gained = PlayerPrefs.GetInt("M_money");
+
+        coinText.GetComponent<Text>().text = (current + gained).ToString();
     }
 }
